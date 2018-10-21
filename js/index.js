31bc45a8137ce38ae9c21721b998c667003492ab
Dominik Zając <zajac_dominik@onet.pl>
2018-10-21 09:46:07 +0200

    Add table of the game

--- js/index.js
+++ js/index.js
@@ -7,7 +7,8 @@ var computerOutcome = document.getElementById('computer-score');
 var drawOutcome = document.getElementById('draw-score');
 var playerOutcome = document.getElementById('user-score');
 var choiceMove = document.querySelectorAll('.player-move');
-var params = {playerScore: '', computerScore: '', drawScore: '', roundOfGame: ''};
+var content = document.getElementById('content');
+var params = {playerScore: '', computerScore: 0, drawScore: '', gameResult: '', roundOfGame: 0, playedRound: '', progress:[]};
 //Funkcja która zostaje wywoływana gdy dana
 //rozgrywka zostanie zakończona poprzez przycisk 
 //'NewGame' - następuje reset wartości
@@ -19,14 +20,18 @@ var reset = function() {
     //lostFinalScore.innerHTML = '';
     //winningFinalScore.innerHTML = '';
 }
+
 //Rozpoczęcie nowej gry z wyborem ilości rund z
 //dodaniem odpowiednich informacji do pozczególnych zadań
 var newGame = function() {
+    params.progress = [];
+    params.playedRound = 0;
     params.playerScore = 0;
     params.computerScore = 0;
     params.drawScore = 0;
     params.roundOfGame = 0;
     params.roundOfGame = window.prompt('How many rounds do you want to play?');
+    scoreTable.innerHTML = '<tr><th>Round</th><th>Player move</th><th>Computer move</th><th>Result</th><th>Score</th></tr>';
     if (params.roundOfGame > 0) {
         reset();
     }
@@ -38,6 +43,30 @@ var newGame = function() {
         numberOfRoundsInformation.innerHTML = 'If you win ' + params.roundOfGame + ' rounds you will be winner!<br>';
     };
 };
+var progress = function(playerChoice, computerMove, gameResult) {
+    params.playerChoice = playerChoice;
+    params.computerMove = computerMove;
+    params.gameResult = gameResult;
+
+    var progressGame = {};
+    progressGame['round'] = params.playedRound;
+    progressGame['player'] = params.playerChoice;
+    progressGame['computer'] = params.computerMove;
+    progressGame['result'] = params.gameResult;
+    progressGame['score'] = params.playerScore + '-' + params.computerScore;
+    params.progress.push(progressGame);
+}
+
+var tableWithResults = function() {
+    for (var i = 0; i < params.progress.length; i++) {
+        scoreTable.innerHTML += '<tr><td>' +
+        params.progress[i]['round'] + '</td><td>' +
+        params.progress[i]['player'] + '</td><td>' +
+        params.progress[i]['computer'] + '</td><td>' +
+        params.progress[i]['result'] + '</td><td>' +
+        params.progress[i]['score'] + '</td></tr>';
+    }
+}
 
 //Nadanie wartości poniższym elementom
 //w celu poprawnego działania 'playerMove'
@@ -60,25 +89,34 @@ function valueChoice(value) {
             playerMove(dataMove);
         });
     };
-
+var playerChoice;
 //Dodawanie poszczególnych wyników z informacją
 //zależne od danego ruchu(wygranej/przegranej)
 var playerMove = function(playerChoice) {
-    var playerChoice;
+    var gameResult;
     var computerMove = getComputerMove();
     if (params.computerScore < params.roundOfGame && params.playerScore < params.roundOfGame) {
         if (valueChoice(playerChoice) - valueChoice(computerMove) === -1 || valueChoice(playerChoice) - valueChoice(computerMove) === 2) {
             resultOfTheRoundInformation.innerHTML = ('<span style="color:green;">| WIN! |</span> you played: ' + playerChoice + ' computer played: ' + computerMove);
+            gameResult = '<strong><span style="color:#27ae60;">YOU WON!</span></strong>';
             params.playerScore++;
+            params.playedRound++;
+            progress(playerChoice, computerMove, gameResult);
             playerOutcome.innerHTML = params.playerScore;
         } else if (valueChoice(playerChoice) - valueChoice(computerMove) === 0) {
             resultOfTheRoundInformation.innerHTML = ('<span style="color:yellow;">| DRAW |</span> you played: ' + playerChoice + ' computer played: ' + computerMove);
+            gameResult = '<span style="color:#f1c40f;">DRAW</span>';
             params.drawScore++;
+            params.playedRound++;
+            progress(playerChoice, computerMove, gameResult);
             drawOutcome.innerHTML = params.drawScore;
         } else {
             resultOfTheRoundInformation.innerHTML = ('<span style="color:#c23616;">| LOSS |</span> you played: ' + playerChoice + ' computer played: ' + computerMove);
+            gameResult = '<span style="color:#e74c3c;">YOU LOSE</span>';
             params.computerScore++;
+            params.playedRound++;
             computerOutcome.innerHTML = params.computerScore;
+            progress(playerChoice, computerMove, gameResult); 
         }
         if (params.playerScore == params.roundOfGame || params.computerScore == params.roundOfGame) {
             finishGame();
@@ -91,11 +129,13 @@ var finishGame = function() {
         document.querySelector('.modal p').innerHTML = '<span style="color:green;">YOU WON THE ENTIRE GAME!</span>';
         numberOfRoundsInformation.innerHTML = 'Game over, please press the "New game" button!';
         showModal();
+        tableWithResults();
 
     } else {
         document.querySelector('.modal p').innerHTML = '<span style="color:#c23616;">YOU LOSE THE ENTIRE GAME!</span>';
         numberOfRoundsInformation.innerHTML = 'Game over, please press the "New game" button!';
         showModal();
+        tableWithResults();
 
     }
 };
